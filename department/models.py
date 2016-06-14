c1f4f9bfebeca6fb2de0debee40205019bfa3516
Abhijeet Padhy <abhijeet.padhy@gmail.com>
2016-06-14 07:55:26 +0530

    Creates three models for Department App

--- department/models.py
+++ department/models.py
@@ -3,3 +3,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    Name = models.CharField(max_length = 250)
+    Professor_file = models.FileField()
+
+class Session(models.Model):
+    Start = models.IntegerField()
+    End = models.IntegerField()
+    Student_file = models.FileField()
+    Department = models.ForeignKey(Department, on_delete = models.CASCADE)
+
+class Result(models.Model):
+    Session = models.ForeignKey(Session, on_delete = models.CASCADE)
+    Department = models.ForeignKey(Department, on_delete = models.CASCADE)
+    Semester = models.CharField(max_length = 3)
+    Result_file = models.FileField()
