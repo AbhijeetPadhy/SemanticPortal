481f6c707ed908052ebe24f3ebc5386d1798c357
Abhijeet Padhy <abhijeet.padhy@gmail.com>
2016-06-15 13:05:42 +0530

    created view for detail

--- department/views.py
+++ department/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+from .models import Department
 
 # Create your views here.
 def index(request):
-    return render(request, 'department/index.html')
+    all_departments = Department.objects.all()
+    context = { 'all_departments' : all_departments }
+    return render(request, 'department/index.html', context)
+
+def detail(request, department_Name):
+    dep = get_object_or_404(Department, Name=department_Name)
+    context = { 'dep' : dep }
+    return render(request, 'department/detail.html', context)
