d69afb8f8f6db7b007191e1b471cec2839de61f1
Abhijeet Padhy <abhijeet.padhy@gmail.com>
2016-06-17 03:16:53 +0530

    Included the commands to parse rdf files

--- department/views.py
+++ department/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Department
 
+#for rdf
+import rdflib
+import rdflib.graph as g
+import rdfextras
+
 # Create your views here.
 def index(request):
     all_departments = Department.objects.all()
@@ -9,5 +14,22 @@ def index(request):
 
 def detail(request, department_Name):
     dep = get_object_or_404(Department, Name=department_Name)
-    context = { 'dep' : dep }
+    rdfextras.registerplugins()
+    g=rdflib.Graph()
+    g.parse(dep.Professor_file)
+    res1 = g.query("""
+    PREFIX nit: <http://semanticportal/department/Biotechnology/prof/>
+
+    SELECT ?name ?email ?research ?designation
+    WHERE {
+            ?x nit:name ?name.
+            ?x nit:email ?email.
+            ?x nit:designation ?designation.
+            ?x nit:research ?research
+        }
+    """)
+
+    context = { 'dep' : dep,
+                'res1' : res1,
+     }
     return render(request, 'department/detail.html', context)
