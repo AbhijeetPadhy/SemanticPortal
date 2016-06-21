bb6a4cdd2b0709f9d9874830c5371f6195528e66
Abhijeet Padhy <abhijeet.padhy@gmail.com>
2016-06-21 16:13:09 +0530

    Created URL for sessions page

--- department/urls.py
+++ department/urls.py
@@ -6,6 +6,10 @@ app_name = 'department'
 urlpatterns = [
     # /department/
     url(r'^$', views.index,name='index'),
+
     # /department/department_Name/
     url(r'^(?P<department_Name>[A-Za-z]+)/$', views.detail, name='detail'),
+
+    # /department/session/
+    url(r'^(?P<department_Name>[A-Za-z]+)/(?P<session_Start>[0-9][0-9][0-9][0-9])/$', views.session, name='session'),
 ]
