36096ebef94527c034f2afcfe5cecd04bfa774f8
Abhijeet Padhy <abhijeet.padhy@gmail.com>
2016-06-21 12:10:33 +0530

    Added __str__() functions to the models

--- department/models.py
+++ department/models.py
@@ -7,14 +7,23 @@ class Department(models.Model):
     Name = models.CharField(max_length = 250)
     Professor_file = models.FileField()
 
+    def __str__(self):
+        return self.Name
+
 class Session(models.Model):
     Start = models.IntegerField()
     End = models.IntegerField()
     Student_file = models.FileField()
     Department = models.ForeignKey(Department, on_delete = models.CASCADE)
 
+    def __str__(self):
+        return str(self.Start) + "-" + str(self.End) + "-" + str(self.Department)
+
 class Result(models.Model):
     Session = models.ForeignKey(Session, on_delete = models.CASCADE)
     Department = models.ForeignKey(Department, on_delete = models.CASCADE)
     Semester = models.CharField(max_length = 3)
     Result_file = models.FileField()
+
+    def __str__(self):
+        return str(self.Session) + "-" + str(self.Department) + "-" + str(self.Semester)
