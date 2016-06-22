aeacfc6ec7ba0e448e89706520822802a41fae1e
Abhijeet Padhy <abhijeet.padhy@gmail.com>
2016-06-22 20:55:56 +0530

    Created views for results page and display page

--- department/views.py
+++ department/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Department, Session
+from .models import Department, Session, Result
 
 #for rdf
 import rdflib
@@ -69,3 +69,50 @@ def session(request,department_Name,session_Start):
                 'session' : session,
      }
     return render(request, 'department/session.html', context)
+
+def results(request):
+    return render(request, 'department/results.html')
+
+def display(request):
+    dept_P = request.GET['dept']
+    session_P = request.GET['session']
+    regno_P = request.GET['regno']
+    sem_P = request.GET['sem']
+
+    dep = get_object_or_404(Department, Name=dept_P)
+    session = get_object_or_404(Session, Start=session_P, Department= dep)
+    result = get_object_or_404(Result, Session=session, Department=dep, Semester=sem_P)
+
+    rdfextras.registerplugins()
+    g=rdflib.Graph()
+    g.parse(result.Result_file)
+    S = """
+    PREFIX nit: <http://nitdgp.ac.in/>
+
+    SELECT ?fname ?lname ?roll ?regno ?cgpa ?sgpa ?BT501LG ?BT502LG ?BT503LG ?OELG ?BT551LG ?BT552LG ?CH581LG
+    WHERE {
+            ?x nit:fname ?fname.
+            ?x nit:lname ?lname.
+            ?x nit:regno """+"'"+regno_P+"'"+""".
+            ?x nit:roll ?roll.
+            ?x nit:regno ?regno.
+            ?x nit:cgpa ?cgpa.
+            ?x nit:sgpa ?sgpa.
+            ?x nit:BT501LG ?BT501LG.
+            ?x nit:BT502LG ?BT502LG.
+            ?x nit:BT503LG ?BT503LG.
+            ?x nit:OELG ?OELG.
+            ?x nit:BT551LG ?BT551LG.
+            ?x nit:BT552LG ?BT552LG.
+            ?x nit:CH581LG ?CH581LG
+        }
+    """
+    res1 = g.query(S)
+
+    context = { 'dep' : dep,
+                'res1' : res1,
+                'session' : session,
+                'sem' : sem_P,
+     }
+
+    return render(request, 'department/display.html',context)
